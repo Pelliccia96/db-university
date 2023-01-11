47695c95e1c3dbc5a8231bd9fa7e0f410c828a78
Pelliccia96 <fra.pelliccia96@gmail.com>
2023-01-11 15:39:28 +0100

    completed query SQL

--- query.sql
+++ query.sql
@@ -23,8 +23,8 @@ SELECT * FROM `degrees`
 WHERE `level` = "magistrale";
 
 -- 7. Da quanti dipartimenti è composta l'università? (12)
-SELECT `id` FROM `departments`;
+SELECT COUNT(id) FROM `departments`;
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
-SELECT * FROM `teachers`
+SELECT COUNT(*) FROM `teachers`
 WHERE `phone` IS NULL;
