90f9fc9b54db4623ac927b2d2bf8854ddfcf46b5
Pelliccia96 <fra.pelliccia96@gmail.com>
2023-01-12 19:30:22 +0100

    Adds join sql + BONUS

--- join.sql
+++ join.sql
@@ -11,8 +11,8 @@ WHERE `degrees`.`name` LIKE "%economia%";
 SELECT * FROM `degrees`
 INNER JOIN `departments`
 ON `degrees`.`department_id` = `departments`.`id`
-WHERE `departments`.`name` LIKE "%neuroscienze%";
-
+WHERE `departments`.`name` LIKE "%neuroscienze%"
+AND `degrees`.`level` = "magistrale";
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
 SELECT * FROM `course_teacher`
 INNER JOIN `courses`
@@ -38,15 +38,22 @@ INNER JOIN `teachers`
 ON `course_teacher`.`teacher_id` = `teachers`.`id`
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
-SELECT * FROM `teachers`
+SELECT `teachers`.`name` as `nome`,`teachers`.`surname` as `cognome`, `teachers`.`id`,`departments`.`name` as `dipartimento`
+FROM `teachers`
 INNER JOIN `course_teacher`
-ON `course_teacher`.`teacher_id` = `teachers`.`id`
+ON `teachers`.`id` = `course_teacher`.`teacher_id`
 INNER JOIN `courses`
-ON `courses`.`id` = `course_teacher`.`course_id`
+ON `course_teacher`.`course_id` = `courses`.`id`
 INNER JOIN `degrees`
-ON `degrees`.`id` = `courses`.`degree_id`
+ON `courses`.`degree_id` = `degrees`.`id`
 INNER JOIN `departments`
-ON `departments`.`id` = `degrees`.`department_id`
-WHERE `departments`.`name` LIKE "%matematica%";
+ON `degrees`.`department_id` = `departments`.`id`
+WHERE `departments`.`name` = "Dipartimento di Matematica"
+GROUP BY `teachers`.`id`;
 
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+SELECT `students`.`name` as `nome`,`students`.`surname` as `cognome`, `students`.`id`, COUNT(`students`.`id`) as `tentativi` 
+FROM `students`
+INNER JOIN `exam_student`
+ON `exam_student`.`student_id` = `students`.`id`
+GROUP BY `students`.`id`;
